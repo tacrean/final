5f7cc7bb846867ce780161f7cd5b1684dfdf1fff
tacrean <taimamscrean@gmail.com>
2017-07-26 23:45:20 -0400

    concept 1

---
+++ womensWebsite.py
@@ -0,0 +1,23 @@
+Women's Website
+  possible names:
+    women's world
+    glass breaker
+    slogan -- diamonds are a girls best friend, it makes sense because they break glass so well. 
+
+functions
+  tell women how much they should argue for in a job interview
+    user input their location
+      search database for the average cost of living [https://www.numbeo.com/cost-of-living/region_rankings.jsp?title=2017-mid&region=021]
+      search database for cities with comparable costs of living[cpi deviation of 5]
+    user input their job
+      sear database for job in cities found in the database [https://www.bls.gov/oes/current/oessrcst.htm#top]
+    print the average salary of a person  from the states with the closes cpi's
+  connect them to local and online health resources
+    location of free clinics
+    registration for medicaide
+    link to online birth control
+    planned parenthood website (highlite some educational stuff-- visually do images that link to different aspects of the website)
+    child care resources
+  connect them to opportunities through compiling data from different websites
+    scholarship websites for continuing education
+    link to online degrees and a list of the highest paying work from home jobs to accommodate single mothers
