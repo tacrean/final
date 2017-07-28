3407c709928526005230b521c2aa6bb01eab4d3e
mbrigham19 <mollyrosebrigham@gmail.com>
2017-07-27 21:12:27 -0400

    Update womensWebsite.py

--- womensWebsite.py
+++ womensWebsite.py
@@ -21,3 +21,8 @@ functions
   connect them to opportunities through compiling data from different websites
     scholarship websites for continuing education
     link to online degrees and a list of the highest paying work from home jobs to accommodate single mothers
+  allow women to share stories of sexism or sexual harassment that they have received, along with advice and inspirational messages.
+     all submissions go through the admins, to secure this website as a safe space for all women.
+  dedicate a page simply to inform the general public of the injustices that women face every day (subject of consent, harassment, women in workplace, accessible birth control, abortion, trans women, etc.)
+  a discreet page that appears like a normal website (online shop, idk something like that) that holds discreet buttons that will contact help (911) if a woman is in immediate danger but can't call authorities without alerting their attacker 
+                                       
